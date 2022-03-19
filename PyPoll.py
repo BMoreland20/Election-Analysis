08acfd4073d1033ed7414da891621e07ebe558e3
Blaine Moreland <blaine.l.moreland@gmail.com>
2022-03-19 18:18:54 -0500

    Update code

--- PyPoll.py
+++ PyPoll.py
@@ -22,6 +22,11 @@ candidate_options = []
 #Candidate votes.
 candidate_votes = {}
 
+#Winning Candidate and Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file.
 with open(election_file) as election_data:
     election_reader = csv.reader(election_data)
@@ -51,9 +56,26 @@ for candidate_name in candidate_votes:
     # 2. Retrieve vote count of a candidate.
     votes = candidate_votes[candidate_name]
     # 3. Calculate the percentage of votes.
-    vote_percentage = str(round(float(votes) / float(total_votes) * 100))
+    vote_percentage = float(votes) / float(total_votes) * 100
     # 4. Print the candidate name and percentage of votes.
-    print(f"{candidate_name}: received {vote_percentage}% of the vote.")
+    
+#Winning Candidate and Count
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+#If true then set winning_count = votes and winning_percent = vote_percentage.        
+        winning_count = votes
+        winning_percentage = vote_percentage
+#Set the winning candidate to the candidate's name
+        winning_candidate = candidate_name
+#Print winners name
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
 
 
 # Create a filename variable to a direct or indirect path to the file.
